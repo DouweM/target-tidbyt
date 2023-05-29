7383706b12f9c44138cb589ba0af05580d72c331
Douwe Maan <douwe@selenight.nl>
2023-05-28 18:45:52 -0600

    Initial commit

---
+++ target_tidbyt/sinks.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+"""tidbyt target sink class, which handles writing streams."""
+
+import requests
+
+from singer_sdk.sinks import RecordSink
+
+
+class TidbytSink(RecordSink):
+    """tidbyt target sink class."""
+
+    def process_record(self, record: dict, context: dict) -> None:
+        """Process the record.
+
+        Args:
+            record: Individual record in the stream.
+            context: Stream partition or context dictionary.
+        """
+        # Sample:
+        # ------
+        # client.write(record)  # noqa: ERA001
+
+        if "image_data" not in record:
+            raise ValueError("No image data found in record")
+
+        image_data = record.get("image_data", "")
+
+        token = self._config.get("token")
+        device_id = self._config.get("device_id")
+
+        installation_id = record.get("installation_id")
+        background = record.get("background", True)
+
+        payload = {
+            "image": image_data,
+            "installationID": installation_id,
+            "background": background
+        }
+
+        self.logger.info("Pushing image to Tidbyt device", device_id, payload)
+
+        response = requests.post(
+            "https://api.tidbyt.com/v0/devices/%s/push" % device_id,
+            json=payload,
+            headers={
+                "Authorization": "Bearer %s" % token,
+            }
+        )
+        self.logger.info("Response: %s" % response)
---
+++ target_tidbyt/target.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+"""tidbyt target class."""
+
+from singer_sdk import typing as th
+from singer_sdk.target_base import Target
+
+from target_tidbyt.sinks import (
+    TidbytSink,
+)
+
+
+class TargetTidbyt(Target):
+    """Sample target for tidbyt."""
+
+    name = "target-tidbyt"
+
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "token",
+            th.StringType,
+            description="Tidbyt API token",
+        ),
+        th.Property(
+            "device_id",
+            th.StringType,
+            description="Tidbyt device ID",
+        )
+    ).to_dict()
+
+    default_sink_class = TidbytSink
+
+
+if __name__ == "__main__":
+    TargetTidbyt.cli()
